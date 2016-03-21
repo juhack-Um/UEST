24837be2cfbfd02f2e57d2cb08d6c146ba970832
juhack-Um <jh.um@funzin.co.kr>
2016-03-21 18:38:54 +0900

    azsxcdv

--- UEST/MainWindow.xaml.cs
+++ UEST/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace UEST
             InitializeComponent();
             Console.WriteLine("adsf");
             Console.WriteLine("adsf");
+            Console.WriteLine("adsf");
         }
     }
 }
